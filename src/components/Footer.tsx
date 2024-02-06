e6d4bc05b01b447bd0eb509fddd35adb76a4f483
KareemEhab <kareem8002010@gmail.com>
2024-02-06 18:56:00 +0200

    Add warning tooltip

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,14 +14,23 @@ const Footer = () => {
     <Box>
       <HStack justifyContent="space-between">
         <Box>
-          <FooterButton Icon={VscGitMerge} Content="main" />
-          <FooterButton Icon={VscError} Content="0" />
-          <FooterButton Icon={VscWarning} Content="0" />
+          <FooterButton Icon={VscGitMerge} Content="main" TooltipLabel="" />
+          <FooterButton Icon={VscError} Content="0" TooltipLabel="" />
+
+          <FooterButton
+            Icon={VscWarning}
+            Content="1"
+            TooltipLabel="Disclaimer: The design and idea for this project are not originally mine; however, the implementation has been entirely done by me."
+          />
         </Box>
         <Box>
-          <FooterButton Icon={VscJson} Content="TypeScript JSX" />
-          <FooterButton Icon={VscCheck} Content="Prettier" />
-          <FooterButton Icon={VscBell} Content="" />
+          <FooterButton
+            Icon={VscJson}
+            Content="TypeScript JSX"
+            TooltipLabel=""
+          />
+          <FooterButton Icon={VscCheck} Content="Prettier" TooltipLabel="" />
+          <FooterButton Icon={VscBell} Content="" TooltipLabel="" />
         </Box>
       </HStack>
     </Box>
--- src/components/FooterButton.tsx
+++ src/components/FooterButton.tsx
@@ -1,25 +1,50 @@
-import { Button, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Button,
+  Text,
+  Tooltip,
+  useBreakpointValue,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
 interface Props {
   Icon: IconType;
   Content: string;
+  TooltipLabel: string;
 }
 
-const FooterButton = ({ Icon, Content }: Props) => {
+const FooterButton = ({ Icon, Content, TooltipLabel }: Props) => {
   const iconFontSize = useBreakpointValue({
     base: "10px",
     md: "16px",
   });
   const textFontSize = useBreakpointValue({ base: "8px", md: "13px" });
+  const tooltipBgColor = useColorModeValue("gray.200", "gray.700");
+  const tooltipTextColor = useColorModeValue("gray.800", "gray.200");
 
-  return (
+  // Render the Button component with or without Tooltip based on TooltipLabel
+  const renderButton = () => (
     <Button bg="transparent" borderRadius={0} size="xs">
       <Icon fontSize={iconFontSize} />
       <Text fontSize={textFontSize} marginLeft="4px">
         {Content}
       </Text>
     </Button>
+  );
+
+  // If TooltipLabel is provided, wrap the Button component with Tooltip
+  return TooltipLabel ? (
+    <Tooltip
+      hasArrow
+      label={TooltipLabel}
+      placement="top"
+      bg={tooltipBgColor}
+      color={tooltipTextColor}
+    >
+      {renderButton()}
+    </Tooltip>
+  ) : (
+    renderButton()
   );
 };
 
