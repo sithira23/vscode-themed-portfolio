b165b7fa0ac0f48bbc6cba5b2218828691a5d3c3
KareemEhab <kareem8002010@gmail.com>
2025-06-21 22:46:08 +0300

    Changing the personal Email

--- public/data/contact.ts
+++ public/data/contact.ts
@@ -7,7 +7,7 @@ import {
   FaWhatsapp,
 } from "react-icons/fa";
 
-export const receiver_email = "kareem.e.hamouda@gmail.com"; // Your email for recieving emails using the email service.
+export const receiver_email = "kareem.e.a.hamouda@gmail.com"; // Your email for recieving emails using the email service.
 // The emails are sent using a personally developed email api: https://github.com/KareemEhab/email-sender
 // Feel free to clone the email service as well and deploy your own, you'll find all the steps in the README there.
 // Make sure to update /src/utils/sendEmail.ts with your own deployed link.
